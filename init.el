ec87e387f5e2f6afc176aa4eeb99c196024efd86
Yul3n <yul3n.falx@protonmail.com>
2020-08-13 12:58:52 +0200

    Added fringes.

--- init.el
+++ init.el
@@ -5,3 +5,16 @@
 ;;; Code:
 (org-babel-load-file "~/.emacs.d/config.org")
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c33e247a2074ec855c47d3af487f821687f56cc03a3243209c64ed619c4a9bf3" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
