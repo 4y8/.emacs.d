9dda4d3697bc8704868884dfca8113c1a9f8a874
Yul3n <yul3n.falx@protonmail.com>
2020-08-17 10:42:13 +0200

    Started Common Lisp.

--- init.el
+++ init.el
@@ -5,20 +5,3 @@
 ;;; Code:
 (org-babel-load-file "~/.emacs.d/config.org")
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(anzu use-package-ensure-system-package uncrustify-mode tuareg toc-org smart-tabs-mode rainbow-delimiters projectile offlineimap nord-theme mood-line merlin-eldoc ivy-prescient irony-eldoc git-gutter-fringe general gcmh flycheck-ocaml flycheck-irony evil-magit evil-collection esup elfeed el-patch doom-themes dashboard counsel company-prescient company-irony-c-headers company-irony cmake-font-lock avy auto-package-update)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t nil)))
- '(flycheck-error ((t (:underline "#BF616A"))))
- '(flycheck-info ((t (:underline "#A3BE8C"))))
- '(flycheck-warning ((t (:underline "#EBCB8B"))))
- '(variable-pitch ((t nil))))
